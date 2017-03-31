34b4fcb38899863a9e18fe5b9924b3dad5a4f5a2
Yannick Slegers <yannick.slegers@student.kdg.be>
2017-03-31 11:13:57 +0200

    UnitOfWork

---
+++ Project_Keuzetest/DOMAIN/PersonaVraag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMAIN
+{
+    public class PersonaVraag : Vraag
+    {
+        public string personavraag { get; set; }
+        public List<String> opties { get; set; }
+    }
+}
